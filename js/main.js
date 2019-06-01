803c5f37f6b0b76addeab3b848c98b04e70f1aa9
Norimitsu Takahashi <norimiso.rec@gmail.com>
2019-06-01 17:06:20 +0900

    split and join

--- js/main.js
+++ js/main.js
@@ -5,6 +5,6 @@ function func(){
     for(let i=0;i<temp.length;i++){
         iidxdata[i] = temp[i].split(",");
     }
-
-    document.getElementById("result").innerHTML = iidxdata[100]
+    const result = iidxdata[100].join("\n")
+    document.getElementById("result").innerHTML = result
 }
