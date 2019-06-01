cfc0acb131bf628e3fe0e14c925b9d5b6310f74e
Norimitsu Takahashi <norimiso.rec@gmail.com>
2019-06-01 16:59:37 +0900

    fix type

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function func(){
     const raw_csvdata = document.main_form.iidxcsv.value
     let iidxdata = [];
-    let temp = raw.csvdata.split("\n")
+    let temp = raw_csvdata.split("\n")
     for(let i=0;i<temp.length;i++){
         iidxdata[i] = temp[i].split(",");
     }
